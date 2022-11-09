a7640c2d25298e33acf36a48ecc9341e31a74d3b
julian-dotcom <74622908+julian-dotcom@users.noreply.github.com>
2022-11-09 09:25:46 +0700

    Update ArbDataPuller.py

--- ArbDataPuller.py
+++ ArbDataPuller.py
@@ -80,10 +80,8 @@ class ArbDataPuller:
         print("MAKE SURE THRESHS ARE APPROPRIATE!")
         self.reset_for_new_day()
         sleep_to_desired_interval(self.interval)
-        # while True:
-        #     if determine_if_new_day(self.midnight):
-        for x in range(10):
-            if x == 9:
+        while True:
+            if determine_if_new_day(self.midnight):
                 self.handle_midnight_event()
             self.get_bid_ask_and_process_df_and_test_diff()
             sleep_to_desired_interval(self.interval)
